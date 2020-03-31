842fc1eaebf7b1c6df79d3fe08961f2a56b6fea5
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2020-03-31 14:04:26 +0530

    Updates

---
+++ precache-manifest.76b4bc7464ee04554b2b60f249310845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8856eec7a0310bbe76aa8189b52c33",
+    "url": "/Cocktail-Time/index.html"
+  },
+  {
+    "revision": "3792b5b168171e663395",
+    "url": "/Cocktail-Time/static/css/main.146ee4f0.chunk.css"
+  },
+  {
+    "revision": "52caf670e74f4b958037",
+    "url": "/Cocktail-Time/static/js/2.734c131e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cocktail-Time/static/js/2.734c131e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3792b5b168171e663395",
+    "url": "/Cocktail-Time/static/js/main.b9295213.chunk.js"
+  },
+  {
+    "revision": "2149a879051799a7ea7d",
+    "url": "/Cocktail-Time/static/js/runtime-main.e4d4966f.js"
+  }
+]);
